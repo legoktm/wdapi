da2ef3e9f08d7ac5d2d686f775dbcd234a4d4951
Kunal Mehta <legoktm@gmail.com>
2013-06-16 21:32:30 -0700

    bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='wdapi',
-    version='1.0',
+    version='1.0.1',
     packages=['wdapi'],
     url='https://github.com/legoktm/wdapi',
     license='Public domain',
