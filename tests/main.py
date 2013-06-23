b82f072e47e6a052afc8dcbbd42fbe7341c1ac02
Kunal Mehta <legoktm@gmail.com>
2013-06-23 15:43:52 -0700

    set up a dummy property on test.wikidata.org

--- tests/main.py
+++ tests/main.py
@@ -20,3 +20,22 @@ class TestAPI(unittest.TestCase):
         p = wdapi.WDProperty(self.repo, 'p234234')
         p.get(fetch_text=False)
         self.assertEqual(p.constraints(), {})
+
+    def test_one_of(self):
+        p = wdapi.WDProperty(self.repo, 'p11')
+        p.get(fetch_text=False)
+        self.assertIn('oneof', p.constraints())
+        self.assertIn('q12345', p.constraints()['oneof'])
+
+    def test_single_value(self):
+        p = wdapi.WDProperty(self.repo, 'p11')
+        p.get(fetch_text=False)
+        self.assertIn('single', p.constraints())
+
+    def test_unique_value(self):
+        p = wdapi.WDProperty(self.repo, 'p11')
+        p.get(fetch_text=False)
+        self.assertIn('unique', p.constraints())
+
+if __name__ == "__main__":
+    unittest.main()
