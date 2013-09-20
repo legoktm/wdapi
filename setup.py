2cd43de5c02ce294864a986ecc35230a104714b8
Kunal Mehta <legoktm@gmail.com>
2013-09-19 21:30:01 -0700

    increment version, will push to pypi

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='wdapi',
-    version='1.1.0',
+    version='1.2.0',
     packages=['wdapi'],
     url='https://github.com/legoktm/wdapi',
     license='Public domain',
